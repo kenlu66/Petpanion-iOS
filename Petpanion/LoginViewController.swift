983dfb4c86dbf51d196c7a61938d448c8173687f
Mengying Jin <mewjin@outlook.com>
2024-10-21 18:24:30 -0500

    Added Firebase package and implemented login/resgister

--- Petpanion/LoginViewController.swift
+++ Petpanion/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -13,6 +14,10 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passswordField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var errorMessage: UILabel!
+    
+    let toHomeSegue = "LoginToHome"
+    let toRegisterSegue = "LoginToRegister"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,10 +26,25 @@ class LoginViewController: UIViewController {
         loginButton.layer.masksToBounds = true
         signUpButton.layer.cornerRadius = 27
         signUpButton.layer.masksToBounds = true
+        
+        errorMessage.text = ""
     }
     
     
     @IBAction func loginPressed(_ sender: Any) {
+        guard let userEmail = emailField.text else { return }
+        guard let password = passswordField.text else { return }
+                
+        Auth.auth().signIn(withEmail: userEmail, password: password) { (authResult, error) in
+            if let error = error as NSError? {
+                self.errorMessage.text =  "\(error.localizedDescription)"
+            } else {
+                self.performSegue(withIdentifier: self.toHomeSegue, sender: self)
+            }
+        }
     }
     
+    @IBAction func signUpPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: toRegisterSegue, sender: self)
+    }
 }
--- Petpanion/RegisterViewController.swift
+++ Petpanion/RegisterViewController.swift
@@ -6,24 +6,43 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var errorMessage: UILabel!
+    
+    let toLoginSegue = "RegisterToLogin"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        loginButton.layer.cornerRadius = 27
+        loginButton.layer.masksToBounds = true
+        signUpButton.layer.cornerRadius = 27
+        signUpButton.layer.masksToBounds = true
+        
+        errorMessage.text = ""
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signUpPressed(_ sender: Any) {
+        guard let userEmail = emailField.text else { return }
+        guard let password = passwordField.text else { return }
+                
+        Auth.auth().createUser(withEmail: userEmail, password: password) { (authResult, error) in
+            if let error = error as NSError? {
+                self.errorMessage.text =  "\(error.localizedDescription)"
+            } else {
+                self.performSegue(withIdentifier: self.toLoginSegue, sender: self)
+            }
+        }
     }
-    */
-
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: toLoginSegue, sender: self)
+    }
 }
