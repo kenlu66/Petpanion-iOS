5a55482927dfa6425d27a85686ffc07639661753
Mengying Jin <mewjin@outlook.com>
2024-12-06 00:51:50 -0600

    discarded medical history VC, added options from my health button to go directly to different VC changed pet status UI and instead of daily tracker, it just show the amount of time needed

--- Petpanion/Info Display/Pet/PetStatusViewController.swift
+++ Petpanion/Info Display/Pet/PetStatusViewController.swift
@@ -6,16 +6,19 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 protocol updatePetList {
     func updatePet(pet: Pet, petInd: Int, pList: [Pet], iList: [UIImage])
 }
 
 class PetStatusViewController: UIViewController, updatePetList {
+    
+    let db = Firestore.firestore()
 
     @IBOutlet weak var foodOption: UIButton!
     @IBOutlet weak var waterOption: UIButton!
-    @IBOutlet weak var moodOption: UIButton!
     @IBOutlet weak var playtimeOption: UIButton!
     @IBOutlet weak var myProfileButton: UIButton!
     @IBOutlet weak var myHealthButton: UIButton!
@@ -23,6 +26,13 @@ class PetStatusViewController: UIViewController, updatePetList {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var petEventLabel: UILabel!
     @IBOutlet weak var optionStackView: UIStackView!
+    
+    @IBOutlet weak var foodAmount: UILabel!
+    
+    @IBOutlet weak var waterAmount: UILabel!
+    
+    @IBOutlet weak var PlaytimeNeeded: UILabel!
+    
     var currentPosition = 0
     var delegate: UIViewController!
     
@@ -33,9 +43,19 @@ class PetStatusViewController: UIViewController, updatePetList {
     var petIndex: Int!
     var medicalRecord: MedicalInfo!
     var DailyRecord: DailyReportHistory!
+    var allergies: [MedicalInfo.Record] = []
+    var vaccines: [MedicalInfo.Record] = []
+    var treatments: [MedicalInfo.Record] = []
+    var docIDA: String!
+    var docIDV: String!
+    var docIDT: String!
     
     let petInfoSegue = "PetStatusToPetInfo"
     let medicalSegue = "PetStatusToMedical"
+    let allergySegue = "MedicalToAllergies"
+    let treatmentSegue = "MedicalToTreatment"
+    let vaccineSegue = "MedicalToVaccine"
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,9 +71,6 @@ class PetStatusViewController: UIViewController, updatePetList {
         waterOption.layer.shadowOpacity = 0.25
         waterOption.layer.shadowOffset = CGSize(width: 2, height: 2)
         
-        moodOption.layer.shadowOpacity = 0.25
-        moodOption.layer.shadowOffset = CGSize(width: 2, height: 2)
-        
         playtimeOption.layer.shadowOpacity = 0.25
         playtimeOption.layer.shadowOffset = CGSize(width: 2, height: 2)
         
@@ -62,6 +79,18 @@ class PetStatusViewController: UIViewController, updatePetList {
         
         myHealthButton.layer.shadowOpacity = 0.25
         myHealthButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        
+        foodAmount.text = "\(selectedPet.amountPerMeal * selectedPet.mealsPerDay) g"
+        
+        waterAmount.text = "\(selectedPet.waterNeeded) g"
+        
+        PlaytimeNeeded.text = "\(selectedPet.playtimeNeeded) g"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        fetchRecords()
     }
     
     @IBAction func foodPressed(_ sender: Any) {
@@ -72,13 +101,32 @@ class PetStatusViewController: UIViewController, updatePetList {
         changeViewAnimation(position: 1)
     }
     
-    @IBAction func moodPressed(_ sender: Any) {
+    @IBAction func playtimePressed(_ sender: Any) {
         changeViewAnimation(position: 2)
     }
     
-    @IBAction func playtimePressed(_ sender: Any) {
-        changeViewAnimation(position: 3)
+    @IBAction func healthPressed(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Choose Type", message: nil, preferredStyle: .actionSheet)
+        
+        alertController.addAction(UIAlertAction(title: "Allergy", style: .default, handler: { _ in
+            self.performSegue(withIdentifier: self.allergySegue, sender: nil)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Treatment", style: .default, handler: { _ in
+            self.performSegue(withIdentifier: self.treatmentSegue, sender: nil)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Vaccine", style: .default, handler: { _ in
+            self.performSegue(withIdentifier: self.vaccineSegue, sender: nil)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+        
     }
+    
     
     func changeViewAnimation(position: Int) {
         let screenWidth = UIScreen.main.bounds.width
@@ -109,6 +157,30 @@ class PetStatusViewController: UIViewController, updatePetList {
            let medicalVC = segue.destination as? MedicalHistoryViewController {
             medicalVC.currentPet = selectedPet
         }
+        
+        if segue.identifier == allergySegue,
+           let allergyVC = segue.destination as? AllergiesViewController {
+            allergyVC.allergyList = allergies
+            allergyVC.docID = docIDA
+            allergyVC.pet = selectedPet
+            allergyVC.delegate = self
+        }
+        
+        if segue.identifier == treatmentSegue,
+           let treatmentVC = segue.destination as? TreatmentsViewController {
+            treatmentVC.treatmentList = treatments
+            treatmentVC.docID = docIDT
+            treatmentVC.pet = selectedPet
+            treatmentVC.delegate = self
+        }
+        
+        if segue.identifier == vaccineSegue,
+           let vaccineVC = segue.destination as? VaccinationsViewController {
+            vaccineVC.vaccineList = vaccines
+            vaccineVC.docID = docIDV
+            vaccineVC.pet = selectedPet
+            vaccineVC.delegate = self
+        }
     }
     
     func updatePet(pet: Pet, petInd: Int, pList: [Pet], iList: [UIImage]) {
@@ -119,6 +191,73 @@ class PetStatusViewController: UIViewController, updatePetList {
         self.selectedPet = pet
         if let mainVC = delegate as? changePetList {
             mainVC.updatePet(pet: selectedPet, petInd: petIndex, pList: petList, iList: imageList)
+        }
+    }
+    
+    func fetchRecords() {
+        // Ensure the user is authenticated
+        guard let userId = Auth.auth().currentUser?.uid else {
+            print("User not authenticated.")
+            return
+        }
+        
+        let medicalRef = db.collection("users").document(userId).collection("pets").document(selectedPet.petID).collection("medical records")
+
+        medicalRef.getDocuments { (snapshot, error) in
+            if let error = error {
+                print("Error retrieving pets: \(error.localizedDescription)")
+                return
+            }
+            
+            self.vaccines = []
+            self.allergies = []
+            self.treatments = []
+            
+            for document in snapshot?.documents ?? [] {
+                let medicalData = document.data()
+                
+                guard let type = medicalData["Medical type"] as? String else {
+                    print("Error: Missing medical type in document")
+                    continue
+                }
+                
+                if let docID = medicalData["Document ID"] as? String {
+                    switch type {
+                    case "Allergy":
+                        self.docIDA = docID
+                    case "Treatment":
+                        self.docIDT = docID
+                    case "Vaccine":
+                        self.docIDV = docID
+                    default:
+                        print("error fetching medical document id")
+                    }
+                }
+                print("before reading records")
+                if let records = medicalData["Records"] as? [[String: Any]] {
+                    print("got a list of records")
+                    for record in records {
+                        print("reading record")
+                        if let date = record["date"] as? String,
+                           let description = record["description"] as? String,
+                           let location = record["location"] as? String {
+                            
+                            let data = MedicalInfo.Record(description: description, date: date, location: location)
+                            
+                            switch type {
+                            case "Allergy":
+                                self.allergies.append(data)
+                            case "Treatment":
+                                self.treatments.append(data)
+                            case "Vaccine":
+                                self.vaccines.append(data)
+                            default:
+                                print("error fetching medical records")
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
