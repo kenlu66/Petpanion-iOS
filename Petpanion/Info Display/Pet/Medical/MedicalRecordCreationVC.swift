1694ea37995ec202cc7942b5ce3690a11e5525b9
ruolin111 <ruolin111@utexas.edu>
2024-12-06 00:00:46 -0600

    health record creation map search now working

--- Petpanion/Info Display/Pet/Medical/MedicalRecordCreationVC.swift
+++ Petpanion/Info Display/Pet/Medical/MedicalRecordCreationVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseAuth
+import MapKit
 
 class MedicalRecordCreationVC: UIViewController, UITextFieldDelegate {
 
@@ -14,6 +15,7 @@ class MedicalRecordCreationVC: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var descriptionField: UITextField!
     @IBOutlet weak var locationField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
     
     var currentType: String!
     var delegate: UIViewController!
@@ -25,16 +27,70 @@ class MedicalRecordCreationVC: UIViewController, UITextFieldDelegate {
         dateField.delegate = self
         descriptionField.delegate = self
         locationField.delegate = self
-        
 
+        // Trigger map search if locationField has a value
+        if let locationText = locationField.text, !locationText.isEmpty {
+            performSearch(for: locationText)
+        }
     }
     
-    // MARK: - Keyboard Dismiss
-    // Called when 'return' key pressed
-    func textFieldShouldReturn(_ textField:UITextField) -> Bool {
+    // UITextFieldDelegate Methods
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == locationField, let searchText = textField.text, !searchText.isEmpty {
+            performSearch(for: searchText)
+        }
+    }
+    
+    // Perform Map Search
+    private func performSearch(for query: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = query
+        
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { [weak self] response, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                print("Error searching for location: \(error)")
+                return
+            }
+            
+            guard let response = response, !response.mapItems.isEmpty else {
+                print("No results found for the search query.")
+                return
+            }
+            
+            // Clear existing annotations
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            
+            // Add annotations for the search results
+            for mapItem in response.mapItems {
+                let annotation = MKPointAnnotation()
+                annotation.title = mapItem.name
+                if let location = mapItem.placemark.location {
+                    annotation.coordinate = location.coordinate
+                }
+                self.mapView.addAnnotation(annotation)
+            }
+            
+            // Zoom the map to the first result
+            if let firstResult = response.mapItems.first,
+               let location = firstResult.placemark.location {
+                let region = MKCoordinateRegion(
+                    center: location.coordinate,
+                    latitudinalMeters: 1000,
+                    longitudinalMeters: 1000
+                )
+                self.mapView.setRegion(region, animated: true)
+            }
+        }
+    }
+        
     
     // Called when the user clicks on the view outside of the UITextField
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
